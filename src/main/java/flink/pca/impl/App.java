59814d9a629216eedb5576e55114f106b23f4da0
toprakkiran <guventoprakkiran@gmail.com>
2016-01-05 15:11:06 +0100

    local svd via mtj works

--- src/main/java/flink/pca/impl/App.java
+++ src/main/java/flink/pca/impl/App.java
@@ -1,13 +1,26 @@
 package flink.pca.impl;
 
+import no.uib.cipr.matrix.NotConvergedException;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+	
+	
+	
+	
+    public static void main( String[] args ) throws NotConvergedException
     {
         System.out.println( "Hello World!" );
+        
+        double[][] vals = {  {3,2,2},         
+        		             {2,3,-2}
+        				  };
+        
+        LinearAlgebraOps.calculateSVD(2, 3, vals);
+
     }
 }
