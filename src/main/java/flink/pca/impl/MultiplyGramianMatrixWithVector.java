d5425f5ba3e8d897440580f67b0cfda882697a6f
Biplob Biswas <revolutionisme@gmail.com>
2016-01-06 23:36:54 +0100

    Multiply Gramian matrix with the vector on the fly

---
+++ src/main/java/flink/pca/impl/MultiplyGramianMatrixWithVector.java
@@ -0,0 +1,119 @@
+package flink.pca.impl;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.common.functions.GroupReduceFunction;
+import org.apache.flink.api.common.functions.MapPartitionFunction;
+import org.apache.flink.api.java.DataSet;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.tuple.Tuple3;
+import org.apache.flink.api.java.tuple.Tuple4;
+import org.apache.flink.util.Collector;
+
+public class MultiplyGramianMatrixWithVector {
+
+	public static void main( String[] args ) throws Exception
+    {
+		final ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+
+		//input data
+		DataSet<String> text = env.fromElements(
+				"1,2,3",
+				"1,2,3"
+				);
+		
+		String vectorV = "1,2,3";
+		
+		DataSet<Tuple3<Integer, Integer,Integer>> gramianMatrix = text
+																	.flatMap(new MapMatrix())
+																	.groupBy(0,1)
+																	.reduceGroup(new ReduceMatrix());
+		
+		try {
+			gramianMatrix.print();
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new Exception();
+		}
+		DataSet<Tuple4<Integer, Integer,Integer,Integer>> multiplyByGramianMatrix = gramianMatrix
+																	.mapPartition(new MapGramMatrix(vectorV));
+		
+		multiplyByGramianMatrix
+		.groupBy(0)
+		.reduceGroup(new ReduceMatrix()) //Have to change to reflect only the row vector shown
+		.print(); 
+		
+    }
+    
+    
+    public static final class MapMatrix implements FlatMapFunction<String, Tuple4<Integer, Integer,Integer, Integer>> {
+
+		private static final long serialVersionUID = 1L;
+
+		public void flatMap(String input, Collector<Tuple4<Integer, Integer,Integer, Integer>> out) {
+			// normalize and split the line
+			String[] tokens = input.split(",");
+			  
+			for(int i=0; i < tokens.length; i++) {
+			  for(int j=0; j < tokens.length; j++){//check j should start from 0 
+			    
+				  out.collect(new Tuple4<Integer, Integer,Integer, Integer>(i,j,Integer.parseInt(tokens[i]), Integer.parseInt(tokens[j])));
+			   
+			   } 
+			  }
+		}
+	}
+    
+    public static class ReduceMatrix implements GroupReduceFunction<Tuple4<Integer, Integer,Integer, Integer>,Tuple3<Integer, Integer,Integer>>
+    {
+
+		private static final long serialVersionUID = 1L;
+
+		@Override
+		public void reduce(Iterable<Tuple4<Integer, Integer, Integer, Integer>> inTuple,
+				Collector<Tuple3<Integer, Integer, Integer>> outTuple) throws Exception {
+			int x = 0;
+			int y = 0;
+			int innerProduct = 0;
+			for(Tuple4<Integer, Integer, Integer, Integer> tuple:inTuple)
+			{
+				x = tuple.f0;
+				y = tuple.f1;
+				innerProduct = innerProduct + (tuple.f2*tuple.f3);
+			}
+
+			outTuple.collect(new Tuple3<Integer, Integer, Integer>(x,y,innerProduct));
+		}	
+    }
+    
+    
+    public static final class MapGramMatrix implements MapPartitionFunction<Tuple3<Integer, Integer,Integer>, Tuple4<Integer, Integer,Integer, Integer>> {
+
+		private static final long serialVersionUID = 1L;
+		//DataSet<String> vectorV;
+    	String vectorV;
+    	public MapGramMatrix(String vectorV)
+    	{
+    		this.vectorV = vectorV;
+    	}
+
+		@Override
+		public void mapPartition(Iterable<Tuple3<Integer, Integer, Integer>> values,
+				Collector<Tuple4<Integer, Integer, Integer, Integer>> out) throws Exception {
+			String[] tokens = vectorV.split(",");
+			int vectorValue = 0;
+			System.out.println(tokens.length);
+			for(Tuple3<Integer, Integer, Integer> value:values)
+			{
+				for(int i = 0; i< tokens.length;i++)
+				{
+					vectorValue = Integer.parseInt(tokens[i]);
+					if(value.f1 == i)
+					{
+						out.collect(new Tuple4<Integer, Integer, Integer, Integer>(value.f0,value.f1,value.f2,vectorValue));
+					}
+				}
+			}
+		}
+    }
+
+}
